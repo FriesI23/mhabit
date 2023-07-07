0c8923d01804dbcb214f522cdba1dd9ea5959fd0
Fries_I23 <FriesI23@outlook.com>
2023-07-07 10:53:47 +0800

    feat: Add new targetdays option (#59)
    * feat: Add new target days radio option
    
    - add new optional
    - refactor dialog
    - change parse to tryParse

--- lib/view/for_habit_edit/habit_target_days_picker.dart
+++ lib/view/for_habit_edit/habit_target_days_picker.dart
@@ -29,7 +29,37 @@ Future<int?> showHabitTargetDaysPickerDialog(
   );
 }
 
-enum _HabitTargetDaysType { days21, days66, days180, daysCustom }
+enum _HabitTargetDaysType {
+  days21(_days21),
+  days66(_days66),
+  days120(_days120),
+  days180(_days180),
+  daysCustom(null);
+
+  static const _days21 = 21;
+  static const _days66 = defaultHabitTargetDays;
+  static const _days120 = 120;
+  static const _days180 = 180;
+
+  final int? days;
+
+  const _HabitTargetDaysType(this.days);
+
+  static _HabitTargetDaysType getTargetDaysType(int targetDays) {
+    switch (targetDays) {
+      case _days21:
+        return days21;
+      case _days66:
+        return days66;
+      case _days120:
+        return days120;
+      case _days180:
+        return days180;
+      default:
+        return _HabitTargetDaysType.daysCustom;
+    }
+  }
+}
 
 class HabitTargetDaysPickerDialog extends StatefulWidget {
   final int targetDays;
@@ -48,7 +78,8 @@ class _HabitTargetDaysDialogView extends State<HabitTargetDaysPickerDialog> {
   @override
   void initState() {
     super.initState();
-    selectTargetDaysType = getTargetDaysType(widget.targetDays);
+    selectTargetDaysType =
+        _HabitTargetDaysType.getTargetDaysType(widget.targetDays);
     customTargetDays = selectTargetDaysType != _HabitTargetDaysType.daysCustom
         ? defaultHabitCustomTargetDays
         : widget.targetDays;
@@ -62,52 +93,20 @@ class _HabitTargetDaysDialogView extends State<HabitTargetDaysPickerDialog> {
     super.dispose();
   }
 
-  int get currentTargetDay => getTargetDaysNum(selectTargetDaysType);
+  int get currentTargetDay => selectTargetDaysType.days ?? customTargetDays;
 
-  int getTargetDaysNum(_HabitTargetDaysType t) {
-    switch (t) {
-      case _HabitTargetDaysType.days21:
-        return 21;
-      case _HabitTargetDaysType.days66:
-        return 66;
-      case _HabitTargetDaysType.days180:
-        return 180;
-      case _HabitTargetDaysType.daysCustom:
-        return customTargetDays;
-    }
-  }
-
-  _HabitTargetDaysType getTargetDaysType(int targetDays) {
-    switch (targetDays) {
-      case 21:
-        return _HabitTargetDaysType.days21;
-      case 66:
-        return _HabitTargetDaysType.days66;
-      case 180:
-        return _HabitTargetDaysType.days180;
-      default:
-        return _HabitTargetDaysType.daysCustom;
-    }
-  }
-
-  String _getTargetDayText(int targetDays, L10n? l10n) {
-    return [targetDays, l10n?.habitEdit_targetDays ?? ''].join(" ");
-  }
-
-  void _onRadioChangeCallback(value) {
+  void _onRadioChangeCallback(_HabitTargetDaysType? value) {
     if (value != null) {
       selectTargetDaysType = value;
       Navigator.pop(context, currentTargetDay);
     }
   }
 
-  void _onCustomTargetDaysInputChange(value) {
-    int newValue;
-    try {
-      newValue = int.parse(value);
-    } on FormatException {
-      newValue = defaultHabitTargetDays;
-    }
+  void _onCustomTargetDaysInputChanged(String? value) {
+    int newValue = value == null
+        ? defaultHabitTargetDays
+        : int.tryParse(value) ?? defaultHabitTargetDays;
+
     if (newValue >= maxHabitTargetDays) {
     } else if (newValue <= minHabitTargetDays) {
       newValue = minHabitTargetDays;
@@ -115,17 +114,18 @@ class _HabitTargetDaysDialogView extends State<HabitTargetDaysPickerDialog> {
     customTargetDays = newValue;
   }
 
+  void _onCustomTargetDaysSubmitted(String? value) {
+    if (selectTargetDaysType == _HabitTargetDaysType.daysCustom) {
+      _onRadioChangeCallback(selectTargetDaysType);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     DebugLog.rebuild("dialog targetDays: "
         "$selectTargetDaysType, $customTargetDays | $currentTargetDay");
 
-    final themeData = Theme.of(context);
-    final textTheme = themeData.textTheme;
     final l10n = L10n.of(context);
-
-    final textScaleFactor =
-        math.min(MediaQuery.textScaleFactorOf(context), 1.3);
 
     return LayoutBuilder(
       builder: (context, constraints) => AlertDialog(
@@ -138,84 +138,121 @@ class _HabitTargetDaysDialogView extends State<HabitTargetDaysPickerDialog> {
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               const SizedBox(width: 300),
-              ListTile(
-                visualDensity: VisualDensity.compact,
-                contentPadding: EdgeInsets.zero,
-                leading: Radio(
-                  value: _HabitTargetDaysType.days21,
-                  groupValue: selectTargetDaysType,
-                  onChanged: (value) => _onRadioChangeCallback(value),
-                ),
-                title: Text(_getTargetDayText(21, l10n)),
+              for (var days in _HabitTargetDaysType.values)
+                if (days != _HabitTargetDaysType.daysCustom)
+                  _HabitSpecificTargetDaysTile(
+                    value: days,
+                    groupValue: selectTargetDaysType,
+                    onRadioChanged: _onRadioChangeCallback,
+                  ),
+              _HabitCustomTargetDaysTile(
+                groupValue: selectTargetDaysType,
+                inputController: _inputController,
+                onRadioChanged: _onRadioChangeCallback,
+                onCustomTargetDaysChanged: _onCustomTargetDaysInputChanged,
+                onCustomTargetDaysSubmitted: _onCustomTargetDaysSubmitted,
               ),
-              ListTile(
-                visualDensity: VisualDensity.compact,
-                contentPadding: EdgeInsets.zero,
-                leading: Radio(
-                  value: _HabitTargetDaysType.days66,
-                  groupValue: selectTargetDaysType,
-                  onChanged: (value) => _onRadioChangeCallback(value),
-                ),
-                title: Text(_getTargetDayText(66, l10n)),
-              ),
-              ListTile(
-                visualDensity: VisualDensity.compact,
-                contentPadding: EdgeInsets.zero,
-                leading: Radio(
-                  value: _HabitTargetDaysType.days180,
-                  groupValue: selectTargetDaysType,
-                  onChanged: (value) => _onRadioChangeCallback(value),
-                ),
-                title: Text(_getTargetDayText(180, l10n)),
-              ),
-              ListTile(
-                visualDensity: VisualDensity.compact,
-                contentPadding: EdgeInsets.zero,
-                leading: Radio(
-                  value: _HabitTargetDaysType.daysCustom,
-                  groupValue: selectTargetDaysType,
-                  onChanged: (value) => _onRadioChangeCallback(value),
-                ),
-                title: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Flexible(
-                      child: SizedBox(
-                        height: 30 * textScaleFactor,
-                        width: 48 * textScaleFactor,
-                        child: TextField(
-                          textAlign: TextAlign.center,
-                          controller: _inputController,
-                          keyboardType: const TextInputType.numberWithOptions(
-                              decimal: false, signed: false),
-                          inputFormatters: [
-                            FilteringTextInputFormatter.digitsOnly,
-                            LengthLimitingTextInputFormatter(
-                                maxHabitTargetDays.toString().length)
-                          ],
-                          style: textTheme.bodyLarge,
-                          onChanged: (value) =>
-                              _onCustomTargetDaysInputChange(value),
-                          onSubmitted: (value) {
-                            if (selectTargetDaysType ==
-                                _HabitTargetDaysType.daysCustom) {
-                              _onRadioChangeCallback(selectTargetDaysType);
-                            }
-                          },
-                        ),
-                      ),
-                    ),
-                    if (l10n != null && l10n.habitEdit_targetDays.isNotEmpty)
-                      Expanded(
-                        flex: 3,
-                        child: Text(l10n.habitEdit_targetDays),
-                      ),
-                  ],
-                ),
-              )
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _HabitSpecificTargetDaysTile extends StatelessWidget {
+  final _HabitTargetDaysType value;
+  final _HabitTargetDaysType? groupValue;
+  final ValueChanged<_HabitTargetDaysType?>? onRadioChanged;
+
+  const _HabitSpecificTargetDaysTile({
+    required this.value,
+    this.groupValue,
+    this.onRadioChanged,
+  });
+
+  String _getTargetDayText(int targetDays, L10n? l10n) {
+    return [targetDays, l10n?.habitEdit_targetDays ?? ''].join(" ");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = L10n.of(context);
+
+    return ListTile(
+      visualDensity: VisualDensity.compact,
+      contentPadding: EdgeInsets.zero,
+      leading: Radio<_HabitTargetDaysType>(
+        value: value,
+        groupValue: groupValue,
+        onChanged: onRadioChanged,
+      ),
+      title: Text(_getTargetDayText(value.days!, l10n)),
+    );
+  }
+}
+
+class _HabitCustomTargetDaysTile extends StatelessWidget {
+  final _HabitTargetDaysType? groupValue;
+  final TextEditingController? inputController;
+  final ValueChanged<_HabitTargetDaysType?>? onRadioChanged;
+  final ValueChanged<String?>? onCustomTargetDaysChanged;
+  final ValueChanged<String?>? onCustomTargetDaysSubmitted;
+
+  const _HabitCustomTargetDaysTile({
+    this.groupValue,
+    this.inputController,
+    this.onRadioChanged,
+    this.onCustomTargetDaysChanged,
+    this.onCustomTargetDaysSubmitted,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textScaleFactor =
+        math.min(MediaQuery.textScaleFactorOf(context), 1.3);
+
+    final themeData = Theme.of(context);
+    final textTheme = themeData.textTheme;
+    final l10n = L10n.of(context);
+
+    return ListTile(
+      visualDensity: VisualDensity.compact,
+      contentPadding: EdgeInsets.zero,
+      leading: Radio<_HabitTargetDaysType>(
+        value: _HabitTargetDaysType.daysCustom,
+        groupValue: groupValue,
+        onChanged: onRadioChanged,
+      ),
+      title: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Flexible(
+            child: SizedBox(
+              height: 30 * textScaleFactor,
+              width: 48 * textScaleFactor,
+              child: TextField(
+                textAlign: TextAlign.center,
+                controller: inputController,
+                keyboardType: const TextInputType.numberWithOptions(
+                    decimal: false, signed: false),
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  LengthLimitingTextInputFormatter(
+                      maxHabitTargetDays.toString().length)
+                ],
+                style: textTheme.bodyLarge,
+                onChanged: onCustomTargetDaysChanged,
+                onSubmitted: onCustomTargetDaysSubmitted,
+              ),
+            ),
+          ),
+          if (l10n != null && l10n.habitEdit_targetDays.isNotEmpty)
+            Expanded(
+              flex: 3,
+              child: Text(l10n.habitEdit_targetDays),
+            ),
+        ],
       ),
     );
   }
