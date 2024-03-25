f193e7a61b3d2f736cf0e229169bbdb15936e3d9
Fries_I23 <FriesI23@outlook.com>
2024-03-25 15:23:17 +0800

    feat: add contributor stucture

---
+++ lib/annotation/contributor_converter.dart
@@ -0,0 +1,87 @@
+// Copyright 2024 Fries_I23
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'dart:collection';
+
+import 'package:intl/locale.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+import '../model/contributor.dart';
+
+class ContributorCollectionConverter
+    implements JsonConverter<ContributorCollection, Map<String, dynamic>> {
+  const ContributorCollectionConverter();
+
+  Iterable<ContributorInfo> buildDefinedContributors(Iterable rawData) =>
+      rawData
+          .whereType<Map<String, dynamic>>()
+          .map((e) => ContributorInfo.fromJson(e));
+
+  Iterable<ContributorInfo> buildContributors(Iterable rawData,
+      [Map<int, ContributorInfo> definedContributors = const {}]) sync* {
+    for (var d in rawData) {
+      switch (d) {
+        case int():
+          if (definedContributors.containsKey(d)) yield definedContributors[d]!;
+          break;
+        case Map<String, dynamic>():
+          yield ContributorInfo.fromJson(d);
+        default:
+          continue;
+      }
+    }
+  }
+
+  @override
+  ContributorCollection fromJson(Map<String, dynamic> json) {
+    final rawDefinedContributors = json["_contributor"];
+    final definedContributors =
+        rawDefinedContributors != null && rawDefinedContributors is List
+            ? Map<int, ContributorInfo>.fromIterable(
+                buildDefinedContributors(rawDefinedContributors),
+                key: (e) => (e as ContributorInfo).id!)
+            : const <int, ContributorInfo>{};
+
+    List<ContributorInfo> contributorListbuilder(dynamic rawData) =>
+        rawData != null && rawData is List
+            ? List<ContributorInfo>.unmodifiable(
+                buildContributors(rawData, definedContributors))
+            : const <ContributorInfo>[];
+
+    SplayTreeMap<Locale, List<ContributorInfo>> translationsMapBuilder(
+        dynamic rawData) {
+      final translations = SplayTreeMap<Locale, List<ContributorInfo>>(
+          (a, b) => a.toLanguageTag().compareTo(b.toLanguageTag()));
+      for (var entry in (rawData != null && rawData is Map<String, dynamic>
+              ? rawData
+              : const <String, dynamic>{})
+          .entries) {
+        final locale = Locale.tryParse(entry.key);
+        if (locale == null) continue;
+        final transList = contributorListbuilder(entry.value);
+        if (transList.isNotEmpty) translations[locale] = transList;
+      }
+      return translations;
+    }
+
+    return ContributorCollection(
+      contributors: contributorListbuilder(json["contributors"]),
+      translations: translationsMapBuilder(json["translations"]),
+    );
+  }
+
+  @override
+  Map<String, dynamic> toJson(ContributorCollection object) =>
+      throw UnimplementedError();
+}
---
+++ lib/model/contributor.dart
@@ -0,0 +1,75 @@
+// Copyright 2024 Fries_I23
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'dart:collection';
+
+import 'package:intl/locale.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+import '../annotation/contributor_converter.dart';
+
+part 'contributor.g.dart';
+
+@JsonSerializable(createToJson: false)
+class ContributorInfo {
+  final int? id;
+  final String name;
+  final String? url;
+
+  const ContributorInfo({this.id, required this.name, this.url});
+
+  factory ContributorInfo.fromJson(Map<String, dynamic> json) =>
+      _$ContributorInfoFromJson(json);
+}
+
+abstract interface class Contributors {
+  Iterable<Locale> get locales;
+
+  Iterable<ContributorInfo>? getTranslations(Locale locale);
+  Iterable<ContributorInfo> getContributors();
+
+  const Contributors();
+
+  factory Contributors.fromJson(Map<String, dynamic> json) =>
+      _Contributors(ContributorCollection.fromJson(json));
+}
+
+class _Contributors implements Contributors {
+  final ContributorCollection data;
+
+  const _Contributors(this.data);
+
+  @override
+  Iterable<ContributorInfo> getContributors() => data.contributors;
+
+  @override
+  Iterable<ContributorInfo>? getTranslations(Locale locale) =>
+      data.translations[locale];
+
+  @override
+  Iterable<Locale> get locales => data.translations.keys;
+}
+
+class ContributorCollection {
+  final List<ContributorInfo> contributors;
+  final SplayTreeMap<Locale, List<ContributorInfo>> translations;
+
+  const ContributorCollection({
+    required this.contributors,
+    required this.translations,
+  });
+
+  factory ContributorCollection.fromJson(Map<String, dynamic> json) =>
+      const ContributorCollectionConverter().fromJson(json);
+}
