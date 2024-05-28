947bdeb0e3c6425bdb94f96aa0d9dc8e40d2ae42
weooh <weooh@tst-desktop.zoltanqy.local>
2024-05-28 15:27:25 +0800

    fix: generate template use v3.13.9

--- lib/reminders/notification_service.dart
+++ lib/reminders/notification_service.dart
@@ -73,6 +73,7 @@ abstract interface class NotificationService implements AsyncInitialization {
   factory NotificationService() {
     if (_instance != null) return _instance!;
     if (Platform.isWindows) return _instance = const FakeNotificationService();
+    if (Platform.isLinux) return _instance = const LinuxNotificationService();
     return _instance = const NotificationServiceImpl();
   }
 }
@@ -320,6 +321,36 @@ final class NotificationServiceImpl implements NotificationService {
     await Future.wait(futureList);
     return true;
   }
+}
+
+final class LinuxNotificationService extends NotificationServiceImpl {
+  const LinuxNotificationService();
+
+  //TODO: Lame implementation; plugin doesn't support scheduling on Linux,
+  //      need to find some solutions.
+  @override
+  Future<bool> regrAppReminderInDaily(
+          {required String title,
+          required String subtitle,
+          required TimeOfDay timeOfDay,
+          required NotificationDetails details,
+          Duration? timeout = NotificationServiceImpl.defaultTimeout}) =>
+      Future.value(false);
+
+  //TODO: Lame implementation; plugin doesn't support scheduling on Linux,
+  //      need to find some solutions.
+  @override
+  Future<bool> regrHabitReminder<T>(
+          {required DBID id,
+          required HabitUUID uuid,
+          required String name,
+          String? quest,
+          required HabitReminder reminder,
+          required HabitDate? lastUntrackDate,
+          required NotificationDetails details,
+          DateTime? crtDate,
+          Duration? timeout = NotificationServiceImpl.defaultTimeout}) =>
+      Future.value(false);
 }
 
 final class FakeNotificationService implements NotificationService {
