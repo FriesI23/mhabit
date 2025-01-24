13e3dd3b4b193971056413870e7c2dc9465c092c
Fries_I23 <20661034+FriesI23@users.noreply.github.com>
2025-01-24 08:35:23 +0800

    feat: add 'ignoressl' sync config

---
+++ lib/view/for_app_sync_server_editor/app_sync_server_ignoressl.dart
@@ -0,0 +1,43 @@
+// Copyright 2025 Fries_I23
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../model/app_sync_server.dart';
+import '../../provider/app_sync_server_form.dart';
+
+class AppSyncServerIgnoreSSL extends StatelessWidget {
+  final EdgeInsetsGeometry? contentPadding;
+
+  const AppSyncServerIgnoreSSL({super.key, this.contentPadding});
+
+  @override
+  Widget build(BuildContext context) {
+    final type = context
+        .select<AppSyncServerFormViewModel, AppSyncServerType>((vm) => vm.type);
+    final ignoreSSL =
+        context.select<AppSyncServerFormViewModel, bool?>((vm) => vm.ignoreSSL);
+    return Visibility(
+      visible: type.includeIgnoreSSLField,
+      child: CheckboxListTile.adaptive(
+        contentPadding: contentPadding,
+        title: const Text("Ignore SSL Certificate"),
+        value: ignoreSSL ?? false,
+        onChanged: (value) =>
+            context.read<AppSyncServerFormViewModel>().ignoreSSL = value,
+      ),
+    );
+  }
+}
