b8b6197878c954a0d266bd1331f5a4b381677272
Fries_I23 <20661034+FriesI23@users.noreply.github.com>
2025-02-26 21:32:57 +0800

    feat: add database about sql and operations

---
+++ assets/sql/mh_sync.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS mh_sync (
+    id_ INTEGER PRIMARY KEY AUTOINCREMENT,
+    create_t INTEGER NOT NULL DEFAULT (cast(strftime('%s','now') as int)),
+    modify_t INTEGER NOT NULL DEFAULT (cast(strftime('%s','now') as int)),
+    habit_uuid TEXT UNIQUE REFERENCES mh_habits(uuid),
+    record_uuid TEXT UNIQUE REFERENCES mh_records(uuid),
+    dirty INTEGER NOT NULL DEFAULT 1,
+    last_config_uuid TEXT,
+    last_session_uuid TEXT,
+    last_mark TEXT
+);
