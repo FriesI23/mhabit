68279a877fa8923842ea61edf01f1f557a621842
Fries_I23 <20661034+FriesI23@users.noreply.github.com>
2025-03-18 19:18:19 +0800

    feat: add multi result

--- lib/model/_app_sync_tasks/webdav_app_sync_task_status.dart
+++ lib/model/_app_sync_tasks/webdav_app_sync_task_status.dart
@@ -13,51 +13,76 @@
 // limitations under the License.
 
 import 'app_sync_task.dart';
+import 'webdav_app_sync_models.dart';
 
-enum WebDavAppSyncTaskResultStatus {
-  success,
-  cancelled,
+enum WebDavAppSyncTaskResultStatus { success, cancelled, failed, multi }
+
+enum WebDavAppSyncTaskResultSubStatus {
   timeout,
   error,
-  failed
+  userAction,
+  missingHabitUuid,
+  empty,
 }
 
 class WebDavAppSyncTaskResult implements AppSyncTaskResult {
   final WebDavAppSyncTaskResultStatus status;
+  final WebDavAppSyncTaskResultSubStatus? reason;
 
   @override
   final ({Object? error, StackTrace? trace}) error;
 
   const WebDavAppSyncTaskResult._(
-      {required this.status, Object? error, StackTrace? trace})
+      {required this.status, this.reason, Object? error, StackTrace? trace})
       : error = (error: error, trace: trace);
 
-  const WebDavAppSyncTaskResult.success()
-      : this._(status: WebDavAppSyncTaskResultStatus.success);
+  const WebDavAppSyncTaskResult.success({
+    WebDavAppSyncTaskResultSubStatus? reason,
+  }) : this._(status: WebDavAppSyncTaskResultStatus.success, reason: reason);
 
-  const WebDavAppSyncTaskResult.failed()
-      : this._(status: WebDavAppSyncTaskResultStatus.failed);
+  const WebDavAppSyncTaskResult.failed({
+    WebDavAppSyncTaskResultSubStatus? reason,
+    Object? error,
+    StackTrace? trace,
+  }) : this._(
+            status: WebDavAppSyncTaskResultStatus.failed,
+            reason: reason,
+            error: error,
+            trace: trace);
 
-  const WebDavAppSyncTaskResult.cancelled({Object? error, StackTrace? trace})
-      : this._(
-          status: WebDavAppSyncTaskResultStatus.cancelled,
-          error: error,
-          trace: trace,
-        );
+  const WebDavAppSyncTaskResult.cancelled({
+    WebDavAppSyncTaskResultSubStatus? reason,
+    Object? error,
+    StackTrace? trace,
+  }) : this._(
+            status: WebDavAppSyncTaskResultStatus.cancelled,
+            reason: reason,
+            error: error,
+            trace: trace);
 
-  const WebDavAppSyncTaskResult.timeout({Object? error, StackTrace? trace})
-      : this._(
-          status: WebDavAppSyncTaskResultStatus.timeout,
-          error: error,
-          trace: trace,
-        );
+  const WebDavAppSyncTaskResult.timeout({
+    Object? error,
+    StackTrace? trace,
+  }) : this.failed(
+            reason: WebDavAppSyncTaskResultSubStatus.timeout,
+            error: error,
+            trace: trace);
 
-  const WebDavAppSyncTaskResult.error({Object? error, StackTrace? trace})
-      : this._(
-          status: WebDavAppSyncTaskResultStatus.error,
-          error: error,
-          trace: trace,
-        );
+  const WebDavAppSyncTaskResult.error({
+    Object? error,
+    StackTrace? trace,
+  }) : this.failed(
+            reason: WebDavAppSyncTaskResultSubStatus.error,
+            error: error,
+            trace: trace);
+
+  factory WebDavAppSyncTaskResult.multi({
+    required Map<WebDavAppSyncHabitInfo, WebDavAppSyncTaskResult> results,
+    Object? error,
+    StackTrace? trace,
+  }) =>
+      WebDavAppSyncTaskMultiResult(
+          habitResults: results, error: error, trace: trace);
 
   @override
   bool get isCancelled => status == WebDavAppSyncTaskResultStatus.cancelled;
@@ -66,13 +91,57 @@ class WebDavAppSyncTaskResult implements AppSyncTaskResult {
   bool get isSuccessed => status == WebDavAppSyncTaskResultStatus.success;
 
   @override
-  bool get isTimeout => status == WebDavAppSyncTaskResultStatus.timeout;
+  bool get isTimeout => reason == WebDavAppSyncTaskResultSubStatus.timeout;
 
   @override
   bool get withError =>
-      status == WebDavAppSyncTaskResultStatus.error || error.error != null;
+      reason == WebDavAppSyncTaskResultSubStatus.error || error.error != null;
 
   @override
   String toString() =>
-      "WebDavAppSyncTaskResult(status=$status, " "error=${error.error})";
+      "WebDavAppSyncTaskResult(status=$status, reason=$reason, "
+      "error=${error.error})";
+}
+
+class WebDavAppSyncTaskMultiResult extends WebDavAppSyncTaskResult {
+  final Map<WebDavAppSyncHabitInfo, WebDavAppSyncTaskResult> habitResults;
+
+  const WebDavAppSyncTaskMultiResult(
+      {super.error, super.trace, this.habitResults = const {}})
+      : super._(status: WebDavAppSyncTaskResultStatus.multi);
+
+  @override
+  bool get isSuccessed {
+    final result = habitResults.values.every((e) => e.isSuccessed);
+    return result ? result : super.isSuccessed;
+  }
+
+  @override
+  bool get isCancelled {
+    final result = habitResults.values.every((e) => e.isCancelled);
+    return result ? result : super.isCancelled;
+  }
+
+  @override
+  bool get isTimeout {
+    final result = habitResults.values.every((e) => e.isTimeout);
+    return result ? result : super.isTimeout;
+  }
+
+  @override
+  String toString() {
+    final counterMap = <(
+      WebDavAppSyncTaskResultStatus,
+      WebDavAppSyncTaskResultSubStatus?
+    ),
+        int>{};
+    for (var entry in habitResults.entries) {
+      final key = (entry.value.status, entry.value.reason);
+      counterMap[key] = (counterMap[key] ?? 0) + 1;
+    }
+
+    return "WebDavAppSyncTaskMultiResult("
+        "error=${error.error}, habits=(all=${habitResults.length}, "
+        "${counterMap.entries.map((e) => "${e.key}=${e.value}").join(", ")}))";
+  }
 }
