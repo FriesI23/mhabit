e32546788948913014662cbf107fd42dcab3cf5b
Fries_I23 <20661034+FriesI23@users.noreply.github.com>
2025-03-23 15:41:13 +0800

    feat: add subdir for sync-failed logs

--- lib/common/consts.dart
+++ lib/common/consts.dart
@@ -31,6 +31,7 @@ const String aboutInfoFilePath = "configs/about_info.json";
 const String debuggerLogFileName = "app_debug.log";
 const String debuggerInfoFileName = "debug.txt";
 const String debuggerZipFile = "debug.zip";
+const String appSyncFailedLogDirSubPath = 'app_sync_failed_logs';
 const String appSyncFailedLogFilePrefix = "app_sync_failed";
 const String appSyncFailedLogFileSuffix = ".txt";
 
