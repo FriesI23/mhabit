1de20887931f9d8b554d02ab6f3619b8d0de69f8
Fries_I23 <20661034+FriesI23@users.noreply.github.com>
2025-03-25 16:58:08 +0800

    feat: hide fetch interval

--- lib/view/page_app_sync.dart
+++ lib/view/page_app_sync.dart
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sliver_tools/sliver_tools.dart';
@@ -170,7 +171,8 @@ class _AppSyncConfigSubgroup extends StatelessWidget {
         children: [
           const Divider(),
           AppSyncSummaryTile(onPressed: onConfigPressed),
-          AppSyncFetchIntervalTile(onPressed: onFetchIntervalPressed),
+          if (kDebugMode)
+            AppSyncFetchIntervalTile(onPressed: onFetchIntervalPressed),
         ],
       ),
     );
