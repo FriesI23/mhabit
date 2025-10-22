fc7aa63c93c544ca04a16ee79b29308117ba9584
Fries_I23 <FriesI23@outlook.com>
2025-10-22 17:17:11 +0800

    refactor(appbar): extract appbar build logic from main page (#382)
    - extract appbar build logic 
    - remove unused widgets

---
+++ lib/pages/habits_display/_widgets/sliver_top_app_bar.dart
@@ -0,0 +1,311 @@
+// Copyright 2025 Fries_I23
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/material.dart';
+import 'package:linked_scroll_controller/linked_scroll_controller.dart';
+import 'package:provider/provider.dart';
+
+import '../../../common/consts.dart';
+import '../../../common/enums.dart';
+import '../../../common/types.dart';
+import '../../../l10n/localizations.dart';
+import '../../../models/habit_stat.dart';
+import '../../../providers/app_compact_ui_switcher.dart';
+import '../../../providers/app_theme.dart';
+import '../../../providers/habit_summary.dart';
+import '../../../providers/habits_record_scroll_behavior.dart';
+import '../../../widgets/widgets.dart';
+import '../widgets.dart';
+
+class SliverTopAppBarContainer {
+  final HabitsRecordScrollBehavior scrollBehavior;
+  final int displayPageOccupyPrt;
+
+  const SliverTopAppBarContainer({
+    required this.scrollBehavior,
+    required this.displayPageOccupyPrt,
+  });
+}
+
+class SliverTopAppBarWrapper extends StatelessWidget {
+  final ValueWidgetBuilder<SliverTopAppBarContainer> builder;
+  final Widget? child;
+
+  const SliverTopAppBarWrapper({super.key, required this.builder, this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    final scrollBehavior = context.select<HabitsRecordScrollBehaviorViewModel,
+        HabitsRecordScrollBehavior>((vm) => vm.scrollBehavior);
+    final displayPageOccupyPrt =
+        context.select<AppThemeViewModel, int>((vm) => vm.displayPageOccupyPrt);
+    return builder(
+        context,
+        SliverTopAppBarContainer(
+          displayPageOccupyPrt: displayPageOccupyPrt,
+          scrollBehavior: scrollBehavior,
+        ),
+        child);
+  }
+}
+
+class SliverViewTopAppBar extends StatelessWidget {
+  final bool isClandarExpanded;
+  final ScrollController? verticalScrollController;
+  final LinkedScrollControllerGroup? horizonalScrollControllerGroup;
+  final ValueChanged<bool>? onCalendarToggleExpandPressed;
+  final VoidCallback? onInfoButtonPressed;
+  final VoidCallback? onMenuButtonPressed;
+  final HabitListTilePhysicsBuilder? scrollPhysicsBuilder;
+
+  const SliverViewTopAppBar({
+    super.key,
+    this.isClandarExpanded = false,
+    this.verticalScrollController,
+    this.horizonalScrollControllerGroup,
+    this.onCalendarToggleExpandPressed,
+    this.onInfoButtonPressed,
+    this.onMenuButtonPressed,
+    this.scrollPhysicsBuilder,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final displayPageOccupyPrt =
+        context.select<AppThemeViewModel, int>((vm) => vm.displayPageOccupyPrt);
+    final vm = context.read<HabitSummaryViewModel>();
+    final compactUI = context.read<AppCompactUISwitcherViewModel>();
+    return _ViewAppBar(
+      scrolledUnderElevation: kCommonEvalation,
+      title: L10nBuilder(
+        builder: (context, l10n) =>
+            l10n != null ? Text(l10n.appName) : const Text(appName),
+      ),
+      bottom: SliverCalendarBar(
+        key: const Key('calendar-bar'),
+        verticalScrollController: verticalScrollController,
+        horizonalScrollControllerGroup: horizonalScrollControllerGroup,
+        startDate: DateChangeProvider.of(context).dateTime,
+        endDate: vm.earliestSummaryDataStartDate?.startDate,
+        isExtended: isClandarExpanded,
+        collapsePrt: displayPageOccupyPrt,
+        height: compactUI.appCalendarBarHeight,
+        itemPadding: compactUI.appCalendarBarItemPadding,
+        onLeftBtnPressed: onCalendarToggleExpandPressed,
+        scrollPhysicsBuilder: scrollPhysicsBuilder,
+      ),
+      onInfoButtonPressed: onInfoButtonPressed,
+      onMenuButtonPressed: onMenuButtonPressed,
+    );
+  }
+}
+
+class SliverEditTopAppBar extends StatelessWidget {
+  final bool isClandarExpanded;
+  final ScrollController? verticalScrollController;
+  final LinkedScrollControllerGroup? horizonalScrollControllerGroup;
+  final VoidCallback? onLeadingButtonPressed;
+  final Widget? action;
+  final HabitListTilePhysicsBuilder? scrollPhysicsBuilder;
+
+  const SliverEditTopAppBar({
+    super.key,
+    this.isClandarExpanded = false,
+    this.verticalScrollController,
+    this.horizonalScrollControllerGroup,
+    this.onLeadingButtonPressed,
+    this.action,
+    this.scrollPhysicsBuilder,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final displayPageOccupyPrt =
+        context.select<AppThemeViewModel, int>((vm) => vm.displayPageOccupyPrt);
+    final vm = context.read<HabitSummaryViewModel>();
+
+    Widget buildAppbarTitle(BuildContext context) {
+      return Selector<HabitSummaryViewModel, int>(
+        selector: (context, vm) => vm.selectedHabitsCount,
+        shouldRebuild: (previous, next) {
+          if (next <= 0) return false;
+          return previous != next;
+        },
+        builder: (context, value, child) => AnimatedSwitcher(
+          duration: kEditModeAppbarAnimateDuration,
+          child: Text(value.toString()),
+        ),
+      );
+    }
+
+    final action = this.action;
+    return _EditAppBar(
+      scrolledUnderElevation: kCommonEvalation,
+      title: buildAppbarTitle(context),
+      bottom: SliverCalendarBar(
+        key: const Key('calendar-bar'),
+        verticalScrollController: verticalScrollController,
+        horizonalScrollControllerGroup: horizonalScrollControllerGroup,
+        startDate: DateChangeProvider.of(context).dateTime,
+        endDate: vm.earliestSummaryDataStartDate?.startDate,
+        isExtended: isClandarExpanded,
+        collapsePrt: displayPageOccupyPrt,
+      ),
+      actions: action != null ? [action] : null,
+      onLeadingButtonPressed: onLeadingButtonPressed,
+    );
+  }
+}
+
+class SliverEditTopAppBarAction extends StatelessWidget {
+  final VoidCallback? onEdit;
+  final VoidCallback? onUnarchive;
+  final VoidCallback? onArchive;
+  final VoidCallback? onSelectAll;
+  final VoidCallback? onClone;
+  final VoidCallback? onExportAll;
+  final VoidCallback? onDelete;
+
+  const SliverEditTopAppBarAction({
+    super.key,
+    this.onEdit,
+    this.onUnarchive,
+    this.onArchive,
+    this.onSelectAll,
+    this.onClone,
+    this.onExportAll,
+    this.onDelete,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = L10n.of(context);
+    final stat =
+        context.select<HabitSummaryViewModel, HabitSummarySelectedStatistic>(
+            (vm) => vm.selectStatistic);
+    return AppBarActions<EditModeActionItemConfig, EditModeActionItemCell>(
+      buttonSwitchAnimateDuration: kEditModeAppbarAnimateDuration,
+      actionConfigs: [
+        EditModeActionItemConfig.edit(
+          visible: stat.selected == 1,
+          text: l10n?.habitDisplay_editButton_tooltip ?? "Edit",
+          callback: onEdit,
+        ),
+        EditModeActionItemConfig.unarchive(
+          visible: stat.archived > 0,
+          text: l10n?.habitDisplay_unarchiveButton_tooltip ?? "Unarchive",
+          callback: onUnarchive,
+        ),
+        EditModeActionItemConfig.archive(
+          visible: stat.activated > 0,
+          text: l10n?.habitDisplay_archiveButton_tooltip ?? "Archive",
+          callback: onArchive,
+        ),
+        EditModeActionItemConfig.selectall(
+          text: l10n?.habitDisplay_editPopMenu_selectAll ?? "Select All",
+          callback: onSelectAll,
+        ),
+        EditModeActionItemConfig.clone(
+          visible: stat.selected == 1,
+          text: l10n?.habitDisplay_editPopMenu_clone ?? "Clone",
+          callback: onClone,
+        ),
+        EditModeActionItemConfig.exportall(
+          text: l10n?.habitDisplay_editPopMenu_export ?? "Export",
+          callback: onExportAll,
+        ),
+        EditModeActionItemConfig.delete(
+          text: l10n?.habitDisplay_editPopMenu_delete ?? 'Delete',
+          callback: onDelete,
+        ),
+      ],
+    );
+  }
+}
+
+class _ViewAppBar extends StatelessWidget {
+  final double? scrolledUnderElevation;
+  final Widget? title;
+  final PreferredSizeWidget? bottom;
+  final VoidCallback? onInfoButtonPressed;
+  final VoidCallback? onMenuButtonPressed;
+
+  const _ViewAppBar({
+    this.scrolledUnderElevation,
+    this.title,
+    this.bottom,
+    this.onInfoButtonPressed,
+    this.onMenuButtonPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = L10n.of(context);
+    return SliverAppBar(
+      floating: true,
+      snap: true,
+      pinned: true,
+      centerTitle: true,
+      scrolledUnderElevation: scrolledUnderElevation,
+      shadowColor: Theme.of(context).colorScheme.shadow,
+      title: title,
+      leading: IconButton(
+        onPressed: onInfoButtonPressed,
+        icon: const Icon(Icons.article_outlined),
+      ),
+      actions: [
+        IconButton(
+          onPressed: onMenuButtonPressed,
+          icon: const Icon(Icons.settings_outlined),
+          tooltip: l10n?.habitDisplay_settingButton_tooltip,
+        )
+      ],
+      bottom: bottom,
+    );
+  }
+}
+
+class _EditAppBar extends StatelessWidget {
+  final double? scrolledUnderElevation;
+  final Widget? title;
+  final PreferredSizeWidget? bottom;
+  final List<Widget>? actions;
+  final VoidCallback? onLeadingButtonPressed;
+
+  const _EditAppBar({
+    this.scrolledUnderElevation,
+    this.title,
+    this.bottom,
+    this.actions,
+    this.onLeadingButtonPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      pinned: true,
+      forceElevated: true,
+      scrolledUnderElevation: scrolledUnderElevation,
+      shadowColor: Theme.of(context).colorScheme.shadow,
+      title: title,
+      centerTitle: false,
+      leading: PageBackButton(
+        reason: PageBackReason.close,
+        onPressed: onLeadingButtonPressed,
+      ),
+      actions: actions,
+      bottom: bottom,
+    );
+  }
+}
--- lib/pages/habits_display/widgets.dart
+++ lib/pages/habits_display/widgets.dart
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export '_widgets/habit_display_appbar_edit_mode.dart';
-export '_widgets/habit_display_appbar_view_mode.dart';
 export '_widgets/habit_display_develop_list.dart';
 export '_widgets/habit_display_edit_mode_action.dart';
 export '_widgets/habit_display_empty_image.dart';
@@ -24,5 +22,6 @@ export '_widgets/habit_display_main_menu.dart';
 export '_widgets/habit_display_sort_type_picker.dart';
 export '_widgets/habit_display_stats_menu.dart';
 export '_widgets/sliver_calendar_bar.dart';
+export '_widgets/sliver_top_app_bar.dart';
 export 'providers.dart';
 export 'styles.dart';
