334d8b804aa31093a2917ced057cc74110c7fc13
Fries_I23 <20661034+FriesI23@users.noreply.github.com>
2025-12-02 15:05:23 +0800

    chore: add post clone script on ios/macos

--- macos/ci_scripts/ci_post_clone.sh
+++ macos/ci_scripts/ci_post_clone.sh
@@ -20,6 +20,6 @@ HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
 brew install cocoapods
 
 # Install CocoaPods dependencies.
-cd ios && pod install # run `pod install` in the `ios` directory.
+cd macos && pod install # run `pod install` in the `ios` directory.
 
 exit 0
