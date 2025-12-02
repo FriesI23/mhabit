9e52355618e6192b33b2783cf8a9aa2f39867ccb
Fries_I23 <20661034+FriesI23@users.noreply.github.com>
2025-12-02 15:34:18 +0800

    chore: add pre build scrtips

---
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Fail this script if any subcommand fails.
+set -e
+
+# The default execution directory of this script is the ci_scripts directory.
+cd "$CI_PRIMARY_REPOSITORY_PATH" # change working directory to the root of your cloned repo.
+
+# Use Flutter from the project's submodule instead of cloning from GitHub.
+export PATH="$PATH:$CI_PRIMARY_REPOSITORY_PATH/.flutter/bin"
+
+# Build
+if [[ -n "$CI_XCODEBUILD_SCHEME" ]]; then
+    echo "Detected scheme: $CI_XCODEBUILD_SCHEME"
+    echo "Running: flutter build ipa --release --flavor $CI_XCODEBUILD_SCHEME"
+    flutter build ipa --release --flavor "$CI_XCODEBUILD_SCHEME"
+else
+    echo "No scheme detected. Running: flutter build ipa --release"
+    flutter build ipa --release
+fi
