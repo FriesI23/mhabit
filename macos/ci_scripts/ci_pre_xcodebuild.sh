0e6df6f358a8a30021ed6fc4cc5c34efc3ea504b
Fries_I23 <20661034+FriesI23@users.noreply.github.com>
2025-12-02 15:47:21 +0800

    chore: update pre build scrtips

--- macos/ci_scripts/ci_pre_xcodebuild.sh
+++ macos/ci_scripts/ci_pre_xcodebuild.sh
@@ -10,11 +10,11 @@ cd "$CI_PRIMARY_REPOSITORY_PATH" # change working directory to the root of your 
 export PATH="$PATH:$CI_PRIMARY_REPOSITORY_PATH/.flutter/bin"
 
 # Build
-if [[ -n "$CI_XCODEBUILD_SCHEME" ]]; then
-    echo "Detected scheme: $CI_XCODEBUILD_SCHEME"
-    echo "Running: flutter build macos --release --flavor $CI_XCODEBUILD_SCHEME"
-    flutter build macos --release --flavor "$CI_XCODEBUILD_SCHEME"
+if [[ -n "$CI_XCODE_SCHEME" ]]; then
+    echo "Detected scheme: $CI_XCODE_SCHEME"
+    echo "Running: flutter build macos --release --flavor $CI_XCODE_SCHEME"
+    flutter build macos --release --flavor "$CI_XCODE_SCHEME" --config-only
 else
     echo "No scheme detected. Running: flutter build macos --release"
-    flutter build macos --release
+    flutter build macos --release --config-only
 fi
