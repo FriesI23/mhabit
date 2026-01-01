1639f3a0b1355d3708e5172805aa6a99211caf89
Fries_I23 <FriesI23@outlook.com>
2026-01-01 21:40:13 +0800

    Chore(l10n): Update Hungarian translations (#468)
    - generate translation files
    - add android app language
    - add darwin app language
    - add windows app language
    - update contributors

--- lib/l10n/localizations.g.dart
+++ lib/l10n/localizations.g.dart
@@ -13,6 +13,7 @@ import 'localizations_es.g.dart';
 import 'localizations_fa.g.dart';
 import 'localizations_fr.g.dart';
 import 'localizations_he.g.dart';
+import 'localizations_hu.g.dart';
 import 'localizations_it.g.dart';
 import 'localizations_ja.g.dart';
 import 'localizations_nb.g.dart';
@@ -115,6 +116,7 @@ abstract class L10n {
     Locale('fa'),
     Locale('fr'),
     Locale('he'),
+    Locale('hu'),
     Locale('it'),
     Locale('ja'),
     Locale('nb'),
@@ -2904,7 +2906,7 @@ class _L10nDelegate extends LocalizationsDelegate<L10n> {
   }
 
   @override
-  bool isSupported(Locale locale) => <String>['ar', 'cs', 'de', 'en', 'es', 'fa', 'fr', 'he', 'it', 'ja', 'nb', 'pl', 'pt', 'ru', 'tr', 'uk', 'vi', 'zh'].contains(locale.languageCode);
+  bool isSupported(Locale locale) => <String>['ar', 'cs', 'de', 'en', 'es', 'fa', 'fr', 'he', 'hu', 'it', 'ja', 'nb', 'pl', 'pt', 'ru', 'tr', 'uk', 'vi', 'zh'].contains(locale.languageCode);
 
   @override
   bool shouldReload(_L10nDelegate old) => false;
@@ -2932,6 +2934,7 @@ L10n lookupL10n(Locale locale) {
     case 'fa': return L10nFa();
     case 'fr': return L10nFr();
     case 'he': return L10nHe();
+    case 'hu': return L10nHu();
     case 'it': return L10nIt();
     case 'ja': return L10nJa();
     case 'nb': return L10nNb();
